26ba3755d2bba12877301704e42bd5fb52ea1a43
anzun0 <zxzimin@naver.com>
2021-03-26 23:42:08 +0900

    도메일 개발

---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+
+    @Test
+    public void 회원가입() throws Exception {
+
+        Member member = new Member();
+        member.setName("Ahn");
+
+        Long saveId = memberService.join(member);
+
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //When
+        memberService.join(member1);
+        memberService.join(member2);
+
+        //Then
+        fail("예외가 발생해햐아 한다.");
+    }
+
+}
